506ede34d1d74919c9e1b809b7ec909512aebe8f
Micah <39250044+micah4thewin@users.noreply.github.com>
2022-11-27 12:54:19 -0800

    Update index.js

--- src/events/model/index.js
+++ src/events/model/index.js
@@ -1,1 +1,37 @@
-
+export const event = {
+  _id: '',
+  set id(data) {
+   this._id = data;
+},
+  get id() {
+   return this._id;
+},
+  _calendarId: '',
+  set calendarId(data) {
+  this._calendarId = data;
+},
+  _title:'',
+  _body: '',
+  _isAllday: '',
+  _start: '',
+  _end: '',
+  _goingDuration: 0,
+  _comingDuration: 0,
+  _location: '',
+  _attendees: [],
+  _category: '',
+  _dueDateClass: '',
+  _recurrenceRule: '',
+  _state: '',
+  _isVisible: true,
+  _isPending: false,
+  _isFocused: false,
+  _isReadOnly: false,
+  _isPrivate: false,
+  _color: '',
+  _backgroundColor: '',
+  _dragBackgroundColor: '',
+  _borderColor: '',
+  _customStyle: {},
+  _raw: [],
+};
